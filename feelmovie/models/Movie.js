e7945d0c5afc45bd4b59fb34ed302b2994d2d136
FannyFerg <53188359+FannyFerg@users.noreply.github.com>
2020-03-17 10:21:35 +0100

    first commit

---
+++ feelmovie/models/Movie.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema(
+  {
+    Title: String,
+    Year: Number,
+    Rated: String,
+    Released: Date,
+    Runtime: String,
+    Genre: [String], 
+    Director: String,
+    Writer:String,
+    Actors:String,
+    Plot:String,
+    Language:String,
+    Country: String,
+    Awards:String,
+    Poster:{ type:String, default: 'https://www.sciencesetavenir.fr/assets/img/2016/03/09/cover-r4x3w1000-57df40df2241f-les-signes-de-la-douleur-chez-le-chat.jpg'},
+    
+  }
+)
+
+const Movie = mongoose.model('Movie', movieSchema);
+module.exports = Movie;
